d110b21a29916653c6f7e1fa077b9510ff2e4f71
Baker Umar <49150727+Baker0707@users.noreply.github.com>
2024-01-15 22:56:01 +0300

    Wallet UI

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {ContactForm} from "./ContactForm";
-import {ContactList} from "./ContactList";
-import {Header} from "./Header";
+import { Wallet } from "./Wallet";
+import { ContactList } from "./ContactList";
+import { Header } from "./Header";
 
 export const App = () => (
   <div>
     <Header />
     <div className="min-h-full">
       <div className="max-w-4xl mx-auto p-2">
+        <Wallet />
         <ContactForm />
         <ContactList />
       </div>
